7aa69cb60a1dd6acde6adf3e44647a9429a633be
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-04-30 16:01:43 -0300

    Linting + Format

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,39 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from "axios";
-
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 interface Introduction {
-  status: string;
-  version:string;
-  message:string;
+    status: string;
+    version: string;
+    message: string;
 }
-
 
 function App() {
-  const apiURL = import.meta.env.VITE_SERVER_API + "/hello";
-  console.log(apiURL)
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [introduction, setIntroduction] = useState<Introduction | null>(null);
-  useEffect(() => {
-    console.log("API URL: " + apiURL)
-    axios.get<Introduction>(apiURL)
-      .then(res => setIntroduction(res.data))
-      .catch(err => setError(err.message))
-      .finally(() => setLoading(false));
-  }, []);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [introduction, setIntroduction] = useState<Introduction | null>(null);
+    useEffect(() => {
+      const apiURL = import.meta.env.VITE_SERVER_API + '/hello';
+        axios
+            .get<Introduction>(apiURL)
+            .then((res) => setIntroduction(res.data))
+            .catch((err) => setError(err.message))
+            .finally(() => setLoading(false));
+    }, []);
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error}</p>
-  return (
-    <>
-      <section id="center">
-        <div>
-          <h1>Welcome!</h1>
-          <h3>{introduction?.message}</h3>
-        </div>
-      </section>
-      <section id="spacer"></section>
-    </>
-  )
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+    return (
+        <>
+            <section id="center">
+                <div>
+                    <h1>Welcome!</h1>
+                    <h3>{introduction?.message}</h3>
+                </div>
+            </section>
+            <section id="spacer"></section>
+        </>
+    );
 }
 
-export default App
+export default App;
--- app/src/tests/App.test.tsx
+++ app/src/tests/App.test.tsx
@@ -2,11 +2,13 @@ import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
 test('shows message from API', async () => {
-  render(<App />);
-  
-  expect(screen.getByText('Loading...')).toBeInTheDocument();
-  
-  await waitFor(() => {
-    expect(screen.getByText('Hello from Financiar backend!')).toBeInTheDocument();
-  });
+    render(<App />);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    await waitFor(() => {
+        expect(
+            screen.getByText('Hello from Financiar backend!'),
+        ).toBeInTheDocument();
+    });
 });
